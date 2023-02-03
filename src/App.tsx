66b0e7047429e6b86241eb766f1c1142e51f15c5
svadim-s <vadimrazer2001@gmail.com>
2023-02-03 18:06:12 +0300

    init and configuration webpack

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { Suspense} from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import "./styles/index.scss"
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async'
+import { MainPageAsync } from './pages/MainPage/MainPage.async'
+import { useTheme } from './theme/useTheme'
+import { classNames } from './helpers/classNames/classNames'
+
+
+const App = () => {
+  const {theme, toogleTheme} = useTheme()
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toogleTheme}>Смена Темы</button>
+      <Link to={'./'}>Главная страница</Link>
+      <Link to={'./about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  )
+}
+
+export default App
