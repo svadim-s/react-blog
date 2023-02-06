46849c64a43563a564bf3c4d5f83ab4ab6446259
svadim-s <vadimrazer2001@gmail.com>
2023-02-06 18:51:39 +0300

    add svg and file loaders, button ui component and theme switcher

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import "./styles/index.scss"
 import { classNames } from 'shared/lib/classNames/classNames'
 import {useTheme} from "app/providers/ThemeProvider"
@@ -7,13 +6,12 @@ import { AppRouter } from './providers/router'
 import { Navbar } from 'widgets/Navbar'
 
 const App = () => {
-  const {theme, toogleTheme} = useTheme()
+  const {theme} = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
       <AppRouter />
-      <button onClick={toogleTheme}>Смена Темы</button>
     </div>
   )
 }
