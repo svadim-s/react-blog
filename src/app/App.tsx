cd0311a5b427889e9d86cc88dd547f24403df243
svadim-s <vadimrazer2001@gmail.com>
2023-02-06 17:44:31 +0300

    Add Navbar component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,16 +4,16 @@ import "./styles/index.scss"
 import { classNames } from 'shared/lib/classNames/classNames'
 import {useTheme} from "app/providers/ThemeProvider"
 import { AppRouter } from './providers/router'
+import { Navbar } from 'widgets/Navbar'
 
 const App = () => {
   const {theme, toogleTheme} = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <button onClick={toogleTheme}>Смена Темы</button>
-      <Link to={'./'}>Главная страница</Link>
-      <Link to={'./about'}>О сайте</Link>
+      <Navbar />
       <AppRouter />
+      <button onClick={toogleTheme}>Смена Темы</button>
     </div>
   )
 }
