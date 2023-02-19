dbfafd8822a9105bd236fabff2d2c8243fdcf2eb
svadim-s <vadimrazer2001@gmail.com>
2023-02-19 14:11:36 +0300

    upgrade sidebar, add button themes

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -4,12 +4,22 @@ import cls from './Button.module.scss'
 
 export enum ThemeButton {
   CLEAR = 'clear',
-  OUTLINE = 'outline'
+  OUTLINE = 'outline',
+  BACKGROUND = 'background',
+  BACKGROUND_INVERTED = 'backgroundInverted',
+}
+
+export enum ButtonSize {
+  M = 'size_m',
+  L = 'size_l',
+  XL = 'size_xl',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string
   theme?: ThemeButton
+  square?: boolean
+  size?: ButtonSize
 }
 
 export const Button: FC<ButtonProps> = (props) => {
@@ -17,13 +27,25 @@ export const Button: FC<ButtonProps> = (props) => {
     children,
     className,
     theme,
+    square,
+    size = ButtonSize.M,
     ...otherProps
   } = props
+
+  const mods: Record<string, boolean> = {
+    [cls.square]: square
+  }
+
+  const additional: string[] = [
+    className,
+    cls[theme],
+    cls[size]
+  ]
 
   return (
     <button
       type='button'
-      className={classNames(cls.Button, {}, [className, cls[theme]])}
+      className={classNames(cls.Button, mods, additional)}
       {...otherProps}
     >
       {children}
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,9 +2,13 @@ import { useState } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher'
 import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher'
-import cls from './Sidebar.module.scss'
-import { Button } from 'shared/ui/Button/Button'
+import { Button, ButtonSize, ThemeButton } from 'shared/ui/Button/Button'
 import { useTranslation } from 'react-i18next'
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+import { RoutePath } from 'shared/config/routeConfig/routeConfig'
+import AboutIcon from 'shared/assets/icons/about.svg'
+import MainIcon from 'shared/assets/icons/main.svg'
+import cls from './Sidebar.module.scss'
 
 interface SidebarProps {
   className?: string
@@ -26,12 +30,43 @@ export const Sidebar = ({ className }: SidebarProps) => {
       <Button
         data-testid="sidebar-toggle"
         onClick={onToggle}
+        className={cls.collapseBtn}
+        theme={ThemeButton.BACKGROUND_INVERTED}
+        size={ButtonSize.L}
+        square
       >
-        {t('Переключить')}
+        {collapsed ? '>' : '<'}
       </Button>
+      <div className={cls.items}>
+        <div className={cls.item}>
+          <AppLink
+            theme={AppLinkTheme.SECONDARY}
+            to={RoutePath.main}
+          >
+            <MainIcon className={cls.icon} />
+            <span className={cls.link}>
+              {t('Главная')}
+            </span>
+          </AppLink>
+        </div>
+        <div className={cls.item}>
+          <AppLink
+            theme={AppLinkTheme.SECONDARY}
+            to={RoutePath.about}
+          >
+            <AboutIcon className={cls.icon} />
+            <span className={cls.link}>
+              {t('О сайте')}
+            </span>
+          </AppLink>
+        </div>
+      </div>
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        <LangSwitcher className={cls.lang} />
+        <LangSwitcher
+          short={collapsed}
+          className={cls.lang}
+        />
       </div>
     </div>
   )
