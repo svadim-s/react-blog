9cc6ba0c504a78d748b5d54cf1da3fb74341ef7f
svadim-s <vadimrazer2001@gmail.com>
2024-01-21 23:45:37 +0300

    add useModal hook, upgrade drawer

---
+++ src/shared/lib/hooks/useModal/useModal.ts
@@ -0,0 +1,53 @@
+import { useTheme } from 'app/providers/ThemeProvider'
+import { MutableRefObject, useCallback, useEffect, useRef, useState } from 'react'
+
+interface UseModalProps {
+  onClose?: () => void
+  isOpen?: boolean
+  animationDelay: number
+}
+
+export function useModal ({ animationDelay, isOpen, onClose }: UseModalProps) {
+  const [isClosing, setIsClosing] = useState(false)
+  const [isMounted, setIsMounted] = useState(false)
+  const timerRef = useRef() as MutableRefObject<ReturnType<typeof setTimeout>>
+
+  const close = useCallback(() => {
+    if (onClose) {
+      setIsClosing(true)
+      timerRef.current = setTimeout(() => {
+        onClose()
+        setIsClosing(false)
+      }, animationDelay)
+    }
+  }, [animationDelay, onClose])
+
+  const onKeyDown = useCallback((e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      close()
+    }
+  }, [close])
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsMounted(true)
+    }
+  }, [isOpen])
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('keydown', onKeyDown)
+    }
+
+    return () => {
+      clearTimeout(timerRef.current)
+      window.removeEventListener('keydown', onKeyDown)
+    }
+  }, [isOpen, onKeyDown])
+
+  return {
+    isClosing,
+    isMounted,
+    close
+  }
+}
