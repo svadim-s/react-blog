fe1025dde63d227489067a9590ad7c91351ecf73
svadim-s <vadimrazer2001@gmail.com>
2024-01-21 21:49:32 +0300

    add drawer, add device detector

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -5,22 +5,48 @@ import { Icon } from 'shared/ui/Icon/Icon'
 import { Popover } from 'shared/ui/Popups'
 import NotificationIcon from 'shared/assets/icons/notification.svg'
 import cls from './NotificationButton.module.scss'
+import { useCallback, useState } from 'react'
+import { Drawer } from 'shared/ui/Drawer/Drawer'
+import { BrowserView, MobileView } from 'react-device-detect'
 
 interface NotificationButtonProps {
   className?: string
 }
 
 export const NotificationButton = ({ className }: NotificationButtonProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const onOpenDrawer = useCallback(() => {
+    setIsOpen(true)
+  }, [])
+
+  const onCloseDrawer = useCallback(() => {
+    setIsOpen(false)
+  }, [])
+
+  const trigger = (
+    <Button onClick={onOpenDrawer} theme={ThemeButton.CLEAR}>
+      <Icon Svg={NotificationIcon} inverted />
+    </Button>
+  )
+
   return (
-    <Popover
-      className={classNames(cls.NotificationButton, {}, [className])}
-      direction='bottom left'
-      trigger={(
-        <Button theme={ThemeButton.CLEAR}>
-          <Icon Svg={NotificationIcon} inverted />
-        </Button>
-      )}>
-      <NotificationList className={cls.notifications} />
-    </Popover>
+    <div>
+      <BrowserView>
+        <Popover
+          className={classNames(cls.NotificationButton, {}, [className])}
+          direction='bottom left'
+          trigger={trigger}>
+          <NotificationList className={cls.notifications} />
+        </Popover>
+      </BrowserView>
+      <MobileView>
+        {trigger}
+        <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+          <NotificationList />
+        </Drawer>
+      </MobileView>
+    </div>
+
   )
 }
--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import { useTheme } from 'app/providers/ThemeProvider'
 import { MutableRefObject, ReactNode, useCallback, useEffect, useRef, useState } from 'react'
 import { classNames, Mods } from 'shared/lib/classNames/classNames'
+import { Overlay } from '../Overlay/Overlay'
 import { Portal } from '../Portal/Portal'
 import cls from './Modal.module.scss'
 
@@ -44,10 +45,6 @@ export const Modal = (props: ModalProps) => {
     }
   }, [closeHandler])
 
-  const onContentClick = (e: React.MouseEvent) => {
-    e.stopPropagation()
-  }
-
   useEffect(() => {
     if (isOpen) {
       setIsMounted(true)
@@ -77,13 +74,9 @@ export const Modal = (props: ModalProps) => {
   return (
     <Portal>
       <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
-        <div className={cls.overlay} onClick={closeHandler}>
-          <div
-            className={cls.content}
-            onClick={onContentClick}
-          >
-            {children}
-          </div>
+        <Overlay onClick={closeHandler} />
+        <div className={cls.content}>
+          {children}
         </div>
       </div>
     </Portal>
