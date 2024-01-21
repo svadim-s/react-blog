b8c74fbbe36e13a574573e18ab25eb6377738b09
svadim-s <vadimrazer2001@gmail.com>
2024-01-21 15:47:04 +0300

    configure babel for ts, add fork ts checker, add custom babel plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -4,6 +4,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { type BuildOptions } from './types/config'
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 import CopyPlugin from 'copy-webpack-plugin'
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
 const CircularDependencyPlugin =
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('circular-dependency-plugin')
@@ -30,6 +31,15 @@ export function buildPlugins ({ paths, isDev, apiUrl, project }: BuildOptions): 
       patterns: [
         { from: paths.locales, to: paths.buildLocales }
       ]
+    }),
+    new ForkTsCheckerWebpackPlugin({
+      typescript: {
+        diagnosticOptions: {
+          semantic: true,
+          syntactic: true
+        },
+        mode: 'write-references'
+      }
     })
   ]
 
