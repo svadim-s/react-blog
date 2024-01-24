09e8432c15b7fb08947deab391e504481e296d6d
svadim-s <vadimrazer2001@gmail.com>
2024-01-24 17:33:21 +0300

    add documentation for project, add js doc

--- src/shared/lib/hooks/useModal/useModal.ts
+++ src/shared/lib/hooks/useModal/useModal.ts
@@ -6,6 +6,13 @@ interface UseModalProps {
   animationDelay: number
 }
 
+/**
+   * Переиспользуемый хук для модальных компонентов (drawer/modal)
+   * @param animationDelay
+   * @param isOpen
+   * @param onClose
+  */
+
 export function useModal ({ animationDelay, isOpen, onClose }: UseModalProps) {
   const [isClosing, setIsClosing] = useState(false)
   const [isMounted, setIsMounted] = useState(false)
