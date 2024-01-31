707766cd2cd37ebff0891ac8eea77549df1bd600
svadim-s <vadimrazer2001@gmail.com>
2024-01-31 13:43:46 +0300

    outline btn, skeleton, list view article

--- src/shared/ui/redesigned/Text/Text.tsx
+++ src/shared/ui/redesigned/Text/Text.tsx
@@ -15,6 +15,7 @@ interface TextProps {
   variant?: TextVariant
   align?: TextAlign
   size?: TextSize
+  bold?: boolean
 
   'data-testid'?: string
 }
@@ -41,7 +42,8 @@ export const Text = memo((props: TextProps) => {
     variant = 'primary',
     align = 'left',
     size = 'm',
-    'data-testid': dataTestId = 'Text'
+    'data-testid': dataTestId = 'Text',
+    bold
   } = props
 
   const HeaderTag = mapSizeToHeaderTag[size]
@@ -55,7 +57,7 @@ export const Text = memo((props: TextProps) => {
   ]
 
   return (
-    <div className={classNames(cls.Text, {}, additionalClasses)}>
+    <div className={classNames(cls.Text, { [cls.bold]: bold }, additionalClasses)}>
       {title && (
         <HeaderTag
           className={cls.title}
